bdef770f87522fb45c00b5b69cf73c6b01b94cec
Patrik Niebur <patrik@fanaticdesign.co.uk>
2015-05-27 19:30:46 +0100

    Added curly braces

--- pi-blaster.js
+++ pi-blaster.js
@@ -15,14 +15,14 @@ function writeCommand(cmd, callback) {
 	var buffer = new Buffer(cmd + "\n");
 	var fd = fs.open(PI_BLASTER_PATH, "w", undefined, function(err, fd) {
 		if (err) {
-			if (callback && typeof callback == 'function') callback(err);
+			if (callback && typeof callback == 'function') {callback(err)};
 		} else {
 			fs.write(fd, buffer, 0, buffer.length, -1, function(error, written, buffer) {
 				if (error) {
-					if (callback && typeof callback == 'function') callback(error);
+					if (callback && typeof callback == 'function') {callback(error)};
 				} else {
 					fs.close(fd);
-					if (callback && typeof callback == 'function') callback();
+					if (callback && typeof callback == 'function') {callback()};
 				}
 			});
 		}
